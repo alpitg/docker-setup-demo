e900b93a11c5ca52e11932680efaef75c66839b1
alpitg <alpitgajbhiye@gmail.com>
2020-05-08 01:38:57 +0530

    initial set up for map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { LayoutModule } from './layout/layout.module';
+import { AgmCoreModule } from '@agm/core';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LayoutModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/layout/main-layout/main-layout.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-layout',
+  templateUrl: './main-layout.component.html',
+  styleUrls: ['./main-layout.component.scss']
+})
+export class MainLayoutComponent implements OnInit {
+
+  // latitude = -28.68352;
+  // longitude = -147.20785;
+  // mapType = 'satellite';
+
+  texto : string = 'Wenceslau Braz - Cuidado com as cargas';
+  lat: number = -23.8779431;
+  lng: number = -49.8046873;
+  zoom: number = 15;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
